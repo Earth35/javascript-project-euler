255453bb54060847f0866b4a53a1a8c9c639df04
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2015-12-06 12:44:29 +0100

    Improved solution for problem 3, text fields now submit on Enter

--- scripts/main.js
+++ scripts/main.js
@@ -33,24 +33,61 @@ function primeFactors () {
 	var number = parseInt(document.getElementById("problem3_input").value);
 	var primeFactorArray = [];
 	var remaining = number;
-	var mainLimit = Math.floor(Math.sqrt(number));
-	for (var n=3; n < mainLimit; n++) {
-		var isPrime = true;
-		var primeLimit = Math.floor(Math.sqrt(n));
-		for (var i=2; i<primeLimit; i++) {
-			if (n%i === 0) {
-				isPrime = false;
+	if (number < 25) {
+		for (var n=3; n <= number; n++) {
+			var isPrime = true;
+			var primeLimit = Math.floor(Math.sqrt(n));
+			for (var i=2; i<=primeLimit; i++) {
+				if (n%i === 0) {
+					isPrime = false;
+				}
+			}
+			if (isPrime && (number%n === 0)) {
+				primeFactorArray.push(n);
+				remaining = remaining / n;
+				if (remaining === 1) {
+					break;
+				}
 			}
 		}
-		if (isPrime && (number%n === 0)) {
-			primeFactorArray.push(n);
-			remaining = remaining / n;
-			if (remaining === 1) {
-				break;
+	}
+	else {
+		var mainLimit = Math.floor(Math.sqrt(number));
+		for (var n=3; n <= mainLimit; n++) {
+			var isPrime = true;
+			var primeLimit = Math.floor(Math.sqrt(n));
+			for (var i=2; i<=primeLimit; i++) {
+				if (n%i === 0) {
+					isPrime = false;
+				}
+			}
+			if (isPrime && (number%n === 0)) {
+				primeFactorArray.push(n);
+				remaining = remaining / n;
+				if (remaining === 1) {
+					break;
+				}
 			}
 		}
 	}
 	var lastIndex = primeFactorArray.length - 1;
 	var largestPrimeFactor = primeFactorArray[lastIndex];
 	document.getElementById('res3').innerHTML = "Result: " + largestPrimeFactor;
+}
+
+function submitOnEnter (arg) {
+	var initialArg = arg;
+	var targetId;
+	switch(initialArg) {
+		case "problem3":
+			targetId = "problem3_submit";
+			break;
+		case "problem2":
+			targetId = "problem2_submit";
+			break;
+		case "problem1":
+			targetId = "problem1_submit";
+			break;
+	}	
+	if (event.keyCode == 13) document.getElementById(targetId).click()
 }
